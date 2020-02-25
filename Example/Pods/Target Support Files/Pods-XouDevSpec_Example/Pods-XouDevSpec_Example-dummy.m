3abaaa81246671f141ef8a3f8c82ce58fae7757d
Xander Schoeman <Xschoeman@jhb.dvt.co.za>
2020-02-25 10:36:04 +0200

    Initial Commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/Pods-XouDevSpec_Example/Pods-XouDevSpec_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XouDevSpec_Example : NSObject
+@end
+@implementation PodsDummy_Pods_XouDevSpec_Example
+@end
---
+++ Example/Pods/Target Support Files/XouDevSpec/XouDevSpec-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XouDevSpec : NSObject
+@end
+@implementation PodsDummy_XouDevSpec
+@end
