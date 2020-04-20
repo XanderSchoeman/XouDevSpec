094dea7556d6d8f9818145b4bf1ce173baaf7456
Xander Schoeman <Xschoeman@jhb.dvt.co.za>
2020-04-20 16:55:46 +0200

    Re Added missing files

---
+++ XouDevSpec/Classes/features/XouBaseApiProfile/Urls.swift
@@ -0,0 +1,15 @@
+//
+//  Urls.swift
+//  XouDevSpec
+//
+//  Created by Xander Schoeman on 2020/04/20.
+//
+import Foundation
+
+public struct UrlStrings {
+    public let getUsersUrl = "https://xoubase.herokuapp.com/users"
+    public let getUsersLocalUrl = "http://localhost:8080"
+
+    public init() {
+    }
+}
