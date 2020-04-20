2b7fc8efe5352c42ef1306ddfcdf5d4131bc3be7
Xander Schoeman <Xschoeman@jhb.dvt.co.za>
2020-04-20 11:57:00 +0200

    Made suggestion changes from Prateek.

--- features/XouBaseApiLoginAndRegister/Repo/UserModel.swift
+++ features/XouBaseApiLoginAndRegister/Repo/UserModel.swift
@@ -8,29 +8,17 @@
 import Foundation
 
 public struct User: Decodable {
-    public var id: Int
-    public  var username: String
-    public var password: String
-    public var profileImage: String?
-    public var fullName: String
-    public var email: String
-    public var gender: String?
-    public var age: Int
-    public var favouriteCount: Int
-    public var genrePref: String
+    public var id: Int = 0
+    public  var username: String = ""
+    public var password: String = ""
+    public var profileImage: String? = ""
+    public var fullName: String = ""
+    public var email: String = ""
+    public var gender: String? = ""
+    public var age: Int = 0
+    public var favouriteCount: Int = 0
+    public var genrePref: String = ""
 
-    public init() {
-
-        id.self = 0
-        username.self = ""
-        password.self = ""
-        profileImage.self = ""
-        fullName.self = ""
-        email.self = ""
-        gender.self = ""
-        age.self = 0
-        favouriteCount.self = 0
-        genrePref = ""
-        
+    public init() {        
     }
 }
