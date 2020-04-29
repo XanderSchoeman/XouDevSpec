9959502fa597d1fffa3746707bdc3aac07dd6b19
Xander Schoeman <Xschoeman@jhb.dvt.co.za>
2020-04-29 23:40:58 +0200

    Changed old UI tests to pass.

--- XouDevSpec/Classes/Helper/Extensions.swift
+++ XouDevSpec/Classes/Helper/Extensions.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 public extension UIColor {
-     func rgb(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) -> UIColor {
+     static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) -> UIColor {
         return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1.0)
     }
 }
